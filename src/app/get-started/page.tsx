3932731df6ebbee2be3582e62f1dc67a71949e5e
Daniele Di Nunzio <danieledinunzio@Danieles-MacBook-Air.local>
2026-02-21 11:44:46 -0800

    Test: Minimal get-started page to isolate hydration error

--- src/app/get-started/page.tsx
+++ src/app/get-started/page.tsx
@@ -1,5 +1,8 @@
-import SimpleForm from './SimpleForm';
-
 export default function GetStartedPage() {
-  return <SimpleForm />;
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Get Started - Minimal Test</h1>
+      <p>If you can see this without errors, the basic page works.</p>
+    </div>
+  );
 }
